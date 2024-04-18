94da2c8819fce4c2fcf98fd8620024e72fc28dc0
Anton Makarov <ontko@mailfence.com>
2024-04-18 15:43:49 +0300

    Initial setup

---
+++ app/Http/Controllers/GamesController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+
+class GamesController extends Controller
+{
+    public function new()
+    {
+    }
+
+    public function create()
+    {
+    }
+
+    public function view_games()
+    {
+    }
+
+    public function validate_games()
+    {
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\Http\Controllers\GamesController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return redirect('/upload');
+});
+Route::get('/upload', [GamesController::class, 'new']);
+Route::post('/upload', [GamesController::class, 'create']);
+Route::get('/validate', [GamesController::class, 'view_games']);
+Route::post('/validate', [GamesController::class, 'validate_games']);
